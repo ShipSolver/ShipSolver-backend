e6c65bbe6d5bf8295f1a7af808f5cfaf3dd8e664
Dante Mazza <dantemazza1@gmail.com>
2022-07-07 17:27:04 -0400

    message

--- servers/tenant/blueprints/event_driven/ticket.py
+++ servers/tenant/blueprints/event_driven/ticket.py
@@ -158,6 +158,7 @@ def corsify(resp):
     resp = make_response(json.dumps(resp))
     resp.headers['Access-Control-Allow-Origin'] = '*'
     resp.headers['Access-Control-Allow-Headers'] = ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+    trd
     return resp
 
 >>>>>>> Cors header

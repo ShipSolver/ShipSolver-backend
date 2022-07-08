2e0de7ca17b0ec8eaf7a7a12995f3c0f53455707
Richard <rzhang606@gmail.com>
2022-07-07 21:00:04 -0400

    Pickup and inventory milestone tables test

---
+++ servers/tenant/database/milestones_test.sql
@@ -0,0 +1,55 @@
+DROP TYPE TEST_INVENTORY_TICKET_STATUS;
+CREATE TYPE TEST_INVENTORY_TICKET_STATUS AS ENUM('SCANNED_INTO_WAREHOUSE', 'TICKET_CREATED', 'CHECKED_INTO_INVENTORY', 'INCOMPLETE_DELIVERY', 'COMPLETE_DELIVERY', 'APPROVED_POD');
+
+CREATE TYPE TEST_TICKET_APPROVAL_STATUS AS ENUM('REENTRY', 'ENTRY');
+
+CREATE TYPE TEST_DELIVERY_TICKET_STATUS AS ENUM('DELIVERED', 'IN_TRANSIT');
+
+CREATE TYPE TEST_PICKUP_TICKET_STATUS AS ENUM('UNASSIGNED', 'REQUESTED', 'ACCEPTED', 'DECLINED', 'COMPLETED','INCOMPLETE');
+
+
+-- Test Check enum values
+SELECT enum_range(NULL::TEST_PICKUP_TICKET_STATUS);
+
+-- ------------------- --
+-- INVENTORY MILESTONE --
+-- ------------------- --
+DROP TABLE TestInventoryMilestones;
+CREATE TABLE IF NOT EXISTS TestInventoryMilestones (
+    "milestoneId" INT,
+    timestamp INT,
+    "userId" INT,
+    "ticketEventId" INT,
+    "previousTicketStatus" TEST_INVENTORY_TICKET_STATUS,
+    "currentTicketStatus" TEST_INVENTORY_TICKET_STATUS,
+    "approvalStatus" TICKET_APPROVAL_STATUS,
+    PRIMARY KEY("milestoneId"),
+    CONSTRAINT "fk_ticketEventId" FOREIGN KEY ("ticketEventId") REFERENCES TicketEvents("ticketEventId")
+);
+
+-- Test add a TestInventoryMilestones
+INSERT INTO TestInventoryMilestones ("milestoneId", timestamp, "userId", "ticketEventId", "previousTicketStatus", "currentTicketStatus", "approvalStatus")
+VALUES (1, 123456, 1, 1, 'SCANNED_INTO_WAREHOUSE', 'TICKET_CREATED', NULL);
+
+-- ---------------- --
+-- PICKUP MILESTONE --
+-- ---------------- --
+DROP TABLE TestPickupMilestones;
+CREATE TABLE IF NOT EXISTS TestPickupMilestones (
+    "milestoneId" INT,
+    timestamp INT,
+    "ticketEventId" INT,
+    "requesterId" INT,
+    "requesteeId" INT,
+    "reasonForDecline" VARCHAR(50),
+    "previousTicketStatus" TEST_PICKUP_TICKET_STATUS,
+    "currentTicketStatus" TEST_PICKUP_TICKET_STATUS,
+    "approvalStatus" TICKET_APPROVAL_STATUS,
+    PRIMARY KEY("milestoneId"),
+    CONSTRAINT "fk_ticketEventId" FOREIGN KEY ("ticketEventId") REFERENCES TicketEvents("ticketEventId")
+);
+
+-- Test add a TestPickupMilestone
+INSERT INTO TestPickupMilestones ("milestoneId", timestamp, "ticketEventId", "requesterId", "requesteeId", "reasonForDecline", "previousTicketStatus", "currentTicketStatus", "approvalStatus")
+VALUES (1, 123456, 1, 123, 321, NULL, 'UNASSIGNED', 'REQUESTED', NULL);
+

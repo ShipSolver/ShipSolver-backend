edd8c01dd8790ebe69f4566509c03cb0c4b04cf6
Satwik Pattanaik <satwik18@gmail.com>
2023-01-20 13:17:30 -0800

    use localhost db

--- servers/tenant/models/__init__.py
+++ servers/tenant/models/__init__.py
@@ -7,7 +7,7 @@ db_port = os.getenv("DB_PORT", "5432")
 db_name = os.getenv("DB_NAME", "tenant_db")
 db_username = os.getenv("DB_USERNAME", "postgres")
 db_password = os.getenv("DB_PASSWORD", "password")
-db_url = os.getenv("DB_URL", "ship-solver.ccxmktobiszx.ca-central-1.rds.amazonaws.com")
+db_url = os.getenv("DB_URL", "localhost")
 # db_url = os.getenv("DB_URL", "localhost")
 
 cnx_string = f"postgresql://{db_username}:{db_password}@{db_url}:{db_port}/{db_name}"

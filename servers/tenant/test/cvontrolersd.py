648105770fecd0b206dd444befb9818a8af7307b
Stefan <new@macbook-pro.home>
2023-01-29 15:56:34 -0500

    Update:

---
+++ servers/tenant/test/cvontrolersd.py
@@ -0,0 +1,24 @@
+# import sys
+
+# sys.path.insert(0, "..")  # import parent folder
+
+# from controllers.controllerMapper import (
+#     UserController,
+#     CreationMilestonesController,
+#     PickupMilestonesController,
+#     InventoryMilestonesController,
+#     AssignmentMilestonesController,
+#     DeliveryMilestonesController,
+#     TicketController,
+#     IncompleteDeliveryMilestonesController,
+#     CustomerController
+# )
+# creationMilestonesController = CreationMilestonesController()
+# pickupMilestonesController = PickupMilestonesController()
+# inventoryMilestonesController = InventoryMilestonesController()
+# assignmentMilestonesController = AssignmentMilestonesController()
+# deliveryMilestonesController = DeliveryMilestonesController()
+# incompleteDeliveryMilestonesController = IncompleteDeliveryMilestonesController()
+# user_controller = UserController()
+# ticket_events_controller = TicketController()
+# customer_controller = CustomerController()

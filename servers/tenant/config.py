b52585ef7751cdd02c62a269550639312d4b8879
Stefan Vercillo <svercillo7@gmail.com>
2023-03-05 23:09:18 -0500

    Add caching config

--- servers/tenant/config.py
+++ servers/tenant/config.py
@@ -1,5 +1,6 @@
 from flask import Flask, make_response
 from celery_client import client
+from flask_caching import Cache
 
 CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
@@ -9,4 +10,8 @@ app.config["DEBUG"] = True
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 app.config["CELERY_BROKER_URL"] = CELERY_BROKER_URL
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CACHE_TYPE'] = 'SimpleCache'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 300
 client.conf.update(app.config)
+
+cache = Cache(app)
